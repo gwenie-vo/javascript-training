2fa3c22a9751d096aa25b3a317e60d95e0b88d48
HuynhVo <gwenie.vo105@gmail.com>
2017-04-13 22:20:13 +0700

    Bai 7

--- object.js
+++ object.js
@@ -41,4 +41,26 @@ for (var i = 0, size = engineers.length; i < size; i++) {
 
 console.log('Bai 4: ' + peopleLovePink + ' love pink');
 
-//
+////Tìm những người là nam và thích màu 'pink'
+var manLovePink = [];
+for (var i = 0, size = engineers.length; i < size; i++) {
+  var engineer = engineers[i];
+  if (engineer.gender === 'Male') {
+    for (var j = 0, lgt = engineer.favoriteColors.length; j < lgt; j++) {
+      if (engineer.favoriteColors[j] === 'pink') {
+        manLovePink.push(engineer.name);
+        break;
+      }
+    }
+  }
+}
+console.log('Bai 6 ' + manLovePink.join(', ') + (manLovePink.length > 1 ? ' are ' : ' is ') + ' mans and they ' + (manLovePink.length > 1 ? ' love ' : ' loves ') + ' pink')
+
+//Tìm xem có ai 25 tuổi không
+engineer25YearsOld = [];
+for (var i = 0, size = engineers.length; i < size; i++) {
+  if (engineers[i].age === 25) {
+    engineer25YearsOld.push(engineers[i].name);
+  }
+}
+console.log('Bai 7 ' + engineer25YearsOld.join(', ') + (engineer25YearsOld.length > 1 ? ' are ' :  ' is ') + ' 25 years old');
