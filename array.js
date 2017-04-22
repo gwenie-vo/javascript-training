2a3712d74b062b84608963cdeff2a9c6d1d180ce
HuynhVo <gwenie.vo105@gmail.com>
2017-04-22 17:15:08 +0700

    Bai 26

--- array.js
+++ array.js
@@ -1,5 +1,14 @@
-//Tìm ra những ai làm ở Asnet
+//25.Tìm ra những ai làm ở Asnet
 var engineerAsNet = engineers.filter(function(engineer) {
   return engineer.company.toLowerCase() === 'asnet';
 })
-console.log('Bai 25: Array of AsNet Engineer', engineerAsNet);
+var engineerName = engineerAsNet.map(function(engineer) {
+  return engineer.name;
+})
+console.log('Bai 25: Array of AsNet Engineer', engineerName);
+
+//26.Kiểm tra xem có người lao động nào dưới 18 tuổi hay không
+var anyUnder18yearsOld = engineers.some(function(engineer) {
+  return engineer.age < 18;
+})
+console.log('Bai 26: Any engineer is under 18 years old: ', anyUnder18yearsOld);
